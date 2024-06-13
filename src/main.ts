3a65a3dd894c2d55e850923f9844cfd10a5685dd
tinict <tinnguyensp.ict@gmail.com>
2024-06-13 16:55:06 +0700

    [security]: Setup helmet can help prevent attacks XSS and CSRF protect HTTP

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  app.use(helmet());
   
   await app.listen(3000);
 }
