e8994808be81946989bb8e0cbaebbd9cd7884b26
tinict <tinnguyensp.ict@gmail.com>
2024-06-13 17:25:30 +0700

    [security]: CSURF: Cross site request forgery protect website attack as CSRF or XSRF

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,11 @@ async function bootstrap() {
    * CORS: Cross Origin Resource Sharing
    */
   app.enableCors();
+
+  /**
+   * CSURF: Cross site request forgery protect website attack as CSRF or XSRF
+   */
+  app.use(csurf());
 
   await app.listen(3000);
 }
